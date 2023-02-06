f99312098c87875aaa373352ffc5020e9ad97318
Christy-Varghese <christy821vargese@gmail.com>
2023-02-06 18:22:41 -0500

    [1] Database Changes

--- src/database/sqlite.js
+++ src/database/sqlite.js
@@ -20,6 +20,111 @@ export const dbInit = () => {
   })
 }
 
+// Insert a game into the database
+export const dbGetGames = () => {
+  const db = SQLite.openDatabase("category.db")
+
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `SELECT * FROM games`,
+        [],
+        (_, result) => {
+          const tasks = result.rows._array.map(item => {
+            return { id: item.id, game: item.game }
+          })
+          resolve(tasks)
+        },
+        (_, err) => {
+          reject(err)
+        }
+      )
+    })
+  })
+}
+
+// Insert a game into the database
+export const dbInsert = (id, game) => {
+  const db = SQLite.openDatabase("category.db")
+
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `INSERT INTO games (id, game) VALUES (?, ?);`,
+        [id, game],
+        (_, result) => {
+          resolve(result)
+        },
+        (_, err) => {
+          reject(err)
+        }
+      )
+    })
+  })
+}
+
+// Delete a game from the database
+export const dbDelete = id => {
+  const db = SQLite.openDatabase("category.db")
+
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `DELETE FROM games WHERE id = ?;`,
+        [id],
+        (_, result) => {
+          resolve(result)
+        },
+        (_, err) => {
+          reject(err)
+        }
+      )
+    })
+  })
+}
+
+// Dlete all games from the database
+export const dbDeleteAll = () => {
+  const db = SQLite.openDatabase("category.db")
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `DELETE FROM games;`,
+        [],
+        (_, result) => {
+          resolve(result)
+        },
+        (_, err) => {
+          reject(err)
+        }
+      )
+    })
+  })
+}
+
+// Drop the games table
+export const dropTable = () => {
+  const db = SQLite.openDatabase("category.db")
+
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(
+        `DROP TABLE games;`,
+        [],
+        (_, result) => {
+          resolve(result)
+        },
+        (_, err) => {
+          reject(err)
+        }
+      )
+    })
+  })
+}
+
+// =================================================================================================
+
+
 // Open a database for expense which holds the id, expense title , expense amount, expense date and the tag
 export const dbInitExpense = () => {
   const db = SQLite.openDatabase("expenses.db")
@@ -122,106 +227,3 @@ export const dbDeleteAllExpenses = () => {
   })
 }
 
-
-
-// Insert a game into the database
-export const dbGetGames = () => {
-  const db = SQLite.openDatabase("category.db")
-
-  return new Promise((resolve, reject) => {
-    db.transaction(tx => {
-      tx.executeSql(
-        `SELECT * FROM games`,
-        [],
-        (_, result) => {
-          const tasks = result.rows._array.map(item => {
-            return { id: item.id, game: item.game }
-          })
-          resolve(tasks)
-        },
-        (_, err) => {
-          reject(err)
-        }
-      )
-    })
-  })
-}
-
-// Insert a game into the database
-export const dbInsert = (id, game) => {
-  const db = SQLite.openDatabase("category.db")
-
-  return new Promise((resolve, reject) => {
-    db.transaction(tx => {
-      tx.executeSql(
-        `INSERT INTO games (id, game) VALUES (?, ?);`,
-        [id, game],
-        (_, result) => {
-          resolve(result)
-        },
-        (_, err) => {
-          reject(err)
-        }
-      )
-    })
-  })
-}
-
-// Delete a game from the database
-export const dbDelete = id => {
-  const db = SQLite.openDatabase("category.db")
-
-  return new Promise((resolve, reject) => {
-    db.transaction(tx => {
-      tx.executeSql(
-        `DELETE FROM games WHERE id = ?;`,
-        [id],
-        (_, result) => {
-          resolve(result)
-        },
-        (_, err) => {
-          reject(err)
-        }
-      )
-    })
-  })
-}
-
-// Dlete all games from the database
-export const dbDeleteAll = () => {
-  const db = SQLite.openDatabase("category.db")
-  return new Promise((resolve, reject) => {
-    db.transaction(tx => {
-      tx.executeSql(
-        `DELETE FROM games;`,
-        [],
-        (_, result) => {
-          resolve(result)
-        },
-        (_, err) => {
-          reject(err)
-        }
-      )
-    })
-  })
-}
-
-// Drop the games table
-export const dropTable = () => {
-  const db = SQLite.openDatabase("category.db")
-
-  return new Promise((resolve, reject) => {
-    db.transaction(tx => {
-      tx.executeSql(
-        `DROP TABLE games;`,
-        [],
-        (_, result) => {
-          resolve(result)
-        },
-        (_, err) => {
-          reject(err)
-        }
-      )
-    })
-  })
-}
