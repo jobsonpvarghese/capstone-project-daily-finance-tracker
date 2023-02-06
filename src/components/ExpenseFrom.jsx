ca7d4588cf943b35e3f4828860b61f9f16914eb0
jobsonpvarghese <jobsonvarghese1@gmail.com>
2023-02-06 01:36:23 -0500

    add expense fab button and form screen added

--- src/components/ExpenseFrom.jsx
+++ src/components/ExpenseFrom.jsx
@@ -1,10 +1,40 @@
 import { StyleSheet, Text, View } from "react-native"
 import React from "react"
+import { TextInput, Button } from "react-native-paper"
 
-const ExpenseFrom = () => {
+const ExpenseFrom = props => {
+  const { navigation } = props
+
   return (
     <View style={styles.container}>
-      <Text>ExpenseFrom</Text>
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "600",
+          color: "#F94A29",
+          textAlign: "center"
+        }}
+      >
+        Add Expense
+      </Text>
+      <TextInput mode="outlined" label="Expense Title" placeholder="Enter you expense label" />
+      <TextInput mode="outlined" label="Amount" placeholder="$" />
+      <View style={styles.btnArea}>
+        <Button style={{ borderColor: "#F94A29" }} icon="close" textColor="#F94A29" mode="outlined" onPress={() => navigation.navigate("Expenses")}>
+          Cancel
+        </Button>
+        <Button
+          style={{
+            marginLeft: 10,
+            backgroundColor: "#F94A29"
+          }}
+          icon="file"
+          mode="contained"
+          onPress={() => console.log("Pressed")}
+        >
+          Add
+        </Button>
+      </View>
     </View>
   )
 }
@@ -13,6 +43,12 @@ export default ExpenseFrom
 
 const styles = StyleSheet.create({
   container: {
-    margin: 20
+    margin: 20,
+    marginTop: 150
+  },
+  btnArea: {
+    marginTop: 20,
+    display: "flex",
+    flexDirection: "row"
   }
 })
