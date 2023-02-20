4092f77407dd189afafec88fe3e09ab1a0648096
Hitarth Bhartkumar Kakkad <68272757+hitarth-k@users.noreply.github.com>
2023-02-20 13:14:53 -0500

    [#4] Removed apex chart because of fatal bug.

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react"
 import { Button, StyleSheet, Text, View } from "react-native"
-// import apex chart
-import Chart from "react-apexcharts"
 
 
 const Home = props => {
@@ -13,36 +11,7 @@ const Home = props => {
         Content for the <Text style={styles.bold}>home</Text> component goes here.
       </Text>
       <Button title="Show Details" onPress={() => navigation.navigate("Details")} />
-      <Chart
-      type="pie"
-      width="100%"
-      height="100%"
-      options={{
-        labels: ["Income", "Expense"],
-        dataLabels: {
-          enabled: true
-        },
-        responsive: [
-          {
-            breakpoint: 480,
-            options: {
-              chart: {
-                width: 200
-              },
-              legend: {
-                position: "bottom"
-              }
-            }
-          }
-        ]
-      }}
-      series={[44, 55]}
-      // series={[44, 55, 41, 17, 15]}
-      // series={[44, 55, 41, 17, 15]}
-      // series={[44, 55, 41, 17, 15]}
-      >
-
-      </Chart>
+   
 
     </View>
   )
